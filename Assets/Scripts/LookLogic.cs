4de90ca4552a4553352ce90b2d255400fc658667
HamzaAit <aithssayenehamza@gmail.com>
2020-06-03 16:53:25 +0100

    Updated character model and script.
    Character has arms and an animator (using unity free assets), rotation still needs some work (gun and camera not fully synchronized)

--- Assets/Scripts/LookLogic.cs
+++ Assets/Scripts/LookLogic.cs
@@ -9,11 +9,12 @@ public class LookLogic : MonoBehaviour
     public Transform player;
     float rot = 0f;
     public Transform gun;
+    public float rotSensitivity = 0.9f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -23,11 +24,11 @@ public class LookLogic : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         rot -= mouseY;
-        rot = Mathf.Clamp(rot, -90f, 90f);
+        rot = Mathf.Clamp(rot, -60f, 60f);
         transform.localRotation = Quaternion.Euler(rot, 0f, 0f);
-        /*rot = Mathf.Clamp(rot, -60f, 60f);
-        gun.transform.localRotation = Quaternion.Euler(rot, 0f, 0f);
-        */
+        //rot = Mathf.Clamp(rot, -60f, 60f);
+        gun.transform.localRotation = Quaternion.Euler(rot * rotSensitivity, 0f, 0f);
+        
         player.Rotate(Vector3.up * mouseX);
 
     }
